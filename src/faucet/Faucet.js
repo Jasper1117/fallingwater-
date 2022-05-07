828b37a85d06cbbb72d0c2e07ad6b8b920b450c6
Jasper <jasperxu1117@gmail.com>
2022-05-07 19:16:36 +0800

    resize img

--- src/faucet/Faucet.js
+++ src/faucet/Faucet.js
@@ -3,9 +3,9 @@ import { Parallax } from 'react-scroll-parallax';
 
 import faucet from "../faucet/images/NewF-01.svg"
 import water from "../faucet/images/NewnewWater.svg"
-import ball1 from "../faucet/images/ball1_4.svg"
-import ball2 from "../faucet/images/ball2-2.svg"
-import ring1 from "../faucet/images/ringball-01.svg"
+import ball1 from "../faucet/images/Ball1_4.png"
+import ball2 from "../faucet/images/Ball2_New2-01.png"
+import ring1 from "../faucet/images/ringball-01.png"
 import "./faucet.css"
 
 const Faucet = () => {
@@ -14,7 +14,7 @@ const Faucet = () => {
     <div className='container'>
 
     <div className="faucetContainer">
-    <Parallax translateY={[-2100,5500]} opacity={[1,0.4]} scale={[0.9,-0.2]}>
+      <Parallax translateY={[-2100,5500]} opacity={[1,0.4]} scale={[0.9,-0.2]}>
         <img src={water} className="water" alt=""/>
       </Parallax>
         <Parallax translateY={[10, -20]} >
@@ -26,23 +26,23 @@ const Faucet = () => {
         opacity={[0.7,1]}
         >
           <img src={ball2} className="ball2" alt=""/>
-        </Parallax>
-        <Parallax scale={ [0.28, 0.32]}
-        rotate={[0, 10]}
+      </Parallax>
+      <Parallax scale={ [0.8, 0.3]}
+        rotate={[120, 0]}
         >
           <img src={ball1} className="ball1" alt=""/>
-        </Parallax>
+      </Parallax>
      
      
-        <Parallax scale={ [1, 0.6]}
+      <Parallax scale={ [1, 0.6]}
         opacity={[1,0]}
         >
       <h1 >Scroll</h1>
-       </Parallax>
+      </Parallax>
        <div className="ring">
-       <Parallax rotate={[90, 300]} scale={ [0.5, 0.9]} className="ring1">
+      <Parallax rotate={[90, 300]} scale={ [0.5, 0.9]} className="ring1">
          <img src={ring1} alt="" />
-         </Parallax>
+      </Parallax>
 
        </div>
     </div>
